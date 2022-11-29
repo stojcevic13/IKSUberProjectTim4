52b63aaa6685a0d014b3e86f79b9df13b366686a
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2022-11-29 23:37:07 +0100

    Designed login component

--- IKSUberProjectTim4/src/app/app.module.ts
+++ IKSUberProjectTim4/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { DummyComponent } from './components/dummy/dummy.component'
-
-
+import { DummyComponent } from './components/dummy/dummy.component';
+import { LoginComponent } from './components/login/login.component'
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import {MatOptionModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DummyComponent,
+    LoginComponent,
 
   ],
   imports: [
@@ -22,7 +29,15 @@ import { DummyComponent } from './components/dummy/dummy.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
