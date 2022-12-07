3d02d74b532fe07f77c28622ea36b149cb06362b
Aleksander Mishutkin <106084948+AlexanderMishutkinP@users.noreply.github.com>
2022-12-07 16:13:42 +0100

    footer

---
+++ IKSUberProjectTim4/src/app/components/playmarket-badge/playmarket-badge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'playmarket-badge',
+  templateUrl: './playmarket-badge.component.html',
+  styleUrls: ['./playmarket-badge.component.css']
+})
+export class PlaymarketBadge {
+
+}
