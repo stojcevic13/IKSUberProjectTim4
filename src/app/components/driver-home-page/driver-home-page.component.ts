fa3fbc2bb43ef3de4093d48e0efb14e6fcadbe3c
stojcevic13 <nemanjastojcevic107@gmail.com>
2022-12-22 08:06:27 +0100

    Added driver service and apply to driver-profile-page

--- src/app/components/driver-home-page/driver-home-page.component.ts
+++ src/app/components/driver-home-page/driver-home-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DriverService } from 'src/app/services/driver.service';
+import { Driver } from 'src/app/services/driver.service';
 
 @Component({
   selector: 'driver-home-page',
   templateUrl: './driver-home-page.component.html',
   styleUrls: ['./driver-home-page.component.css']
 })
-export class DriverHomePageComponent {
+export class DriverHomePageComponent implements OnInit  {
+  driver: Driver = {
+    _id: 0,
+    name: '',
+    surname: '',
+    telephoneNumber:'',
+    address:'',
+    email:''
+  }
 
+  
+  constructor(
+    private route: ActivatedRoute,
+    private driverService: DriverService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.driverService
+        .getDriver(+params['driverId'])
+        .subscribe((driver) => (this.driver = driver));
+    });
+  }
 }
--- src/app/components/driver-profile/driver-profile.component.ts
+++ src/app/components/driver-profile/driver-profile.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DriverService } from 'src/app/services/driver.service';
+import { Driver } from 'src/app/services/driver.service';
 
 @Component({
   selector: 'driver-profile',
   templateUrl: './driver-profile.component.html',
   styleUrls: ['./driver-profile.component.css']
 })
-export class DriverProfileComponent {
+export class DriverProfileComponent implements OnInit {
+  driver: Driver = {
+    _id: 0,
+    name: '',
+    surname: '',
+    telephoneNumber:'',
+    address:'',
+    email:''
+  }
 
+  constructor(
+    private route: ActivatedRoute,
+    private driverService: DriverService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.driverService
+        .getDriver(+params['driverId'])
+        .subscribe((driver) => (this.driver = driver));
+    });
+  }
 }
