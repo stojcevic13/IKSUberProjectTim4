77ade92409e140aabdb2e7b6fd980c5d941c60e1
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2022-12-27 13:06:59 +0100

    Implemented showing all vehicles on map for unregistered user

---
+++ src/app/services/vehicle.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/enviroments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class VehicleService {
+
+  private value$ = new BehaviorSubject<any>({});
+  selectedValue$ = this.value$.asObservable();  
+  constructor(private http : HttpClient) {}
+  setValue(test: any) {           // moze String umjesto any
+    this.value$.next(test);
+  }
+
+  getAll():Observable<Vehicle[]>{
+    return this.http.get<Vehicle[]>(environment.apiHost + 'api/vehicle/all');
+  }
+
+}
+
+export interface Vehicle{
+  babyTransport:boolean,
+  petTransport:boolean,
+  passengerSeats:number,
+  licenseNumber:string,
+  model:string,
+  vehicleType:string,
+  currentLocation:LocationVehicle,
+  available:boolean
+}
+
+export interface LocationVehicle{
+  address:string,
+  latitude:number,
+  longitude:number
+}
